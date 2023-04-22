629d4522a8d2b3e5a51cf8e429c263c405b6710b
alex2022cci <alex2022cci@outlook.fr>
2023-04-22 21:59:51 +0200

    add some comments

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const handleOnUp = () => {
   const handleOnMove = e => {
     if(track.dataset.mouseDownAt === "0") return;
     
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-          maxDelta = window.innerWidth / 2;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, /*écart entre la précédente et nouvlee position H de la souris*/
+          maxDelta = window.innerWidth / 2; /* on parcours la moitié de l'écran avec la souris mais 100% du slider */
     
     const percentage = (mouseDelta / maxDelta) * -100,
           nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
           nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+          /*definit le max et le min du slider, pour ne pas dépasser l'écran*/
     
     track.dataset.percentage = nextPercentage;
     
